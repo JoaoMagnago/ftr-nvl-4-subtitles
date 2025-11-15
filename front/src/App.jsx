524a028c2083469704411746821e7b5facd9be88
joao magnago <joaomagnago.wm@gmail.com>
2025-11-15 16:17:33 -0300

    feat: create function to update image caption

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 export const App = () => {
+  const [imgSrc, setImgSrc] = useState(null)
+  const [caption, setCaption] = useState('<Caption>')
+
+  function generateCaption() {
+    setCaption('Novo caption!')
+  }
+
   return (
     <>
       <h1>Caption Generator</h1>
       <div className='url-form'>
-        <input type="text" />
-        <button>Generate</button>
+        <input onChange={(e) => setImgSrc(e.target.value)} />
+        <button onClick={generateCaption}>Generate</button>
       </div>
       <div className='captioned-image'>
-        <img height={200} width={200} style={{ marginBottom: '10px' }} src="" alt="" />
-        <span>Caption</span>
+        <img src={imgSrc} height={200} style={{ marginBottom: '10px' }} />
+        <span>{caption}</span>
       </div>
     </>
   )
