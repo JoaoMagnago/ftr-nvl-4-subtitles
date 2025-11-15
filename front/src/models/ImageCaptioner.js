65b8a10628dbc82cd1e1aee0df256e808b35ba64
joao magnago <joaomagnago.wm@gmail.com>
2025-11-15 16:35:49 -0300

    feat: create model instance using singleton pattern

--- front/src/models/ImageCaptioner.js
+++ front/src/models/ImageCaptioner.js
@@ -1,7 +1,17 @@
-import {pipelone} from '@huggingface/transformers'
+import { pipeline } from '@huggingface/transformers'
 
 export default class ImageCaptioner {
+  static captioner = null
+
+  static async getCaptioner() {
+    if (this.captioner === null) {
+      this.captioner = await pipeline('image-to-text', 'Xenova/vit-gpt2-image-captioning')
+    }
+    return this.captioner
+  }
+
   static generateCaption(imgSrc) {
+    console.log(imgSrc)
     return "Legenda do modelo"
   }
 }
--- front/src/models/api.js
+++ front/src/models/api.js
@@ -1,5 +1,6 @@
 import ImageCaptioner from "./ImageCaptioner";
 
 export default function generateCaption(imgSrc) {
+  ImageCaptioner.getCaptioner()
   return ImageCaptioner.generateCaption(imgSrc)
 }
